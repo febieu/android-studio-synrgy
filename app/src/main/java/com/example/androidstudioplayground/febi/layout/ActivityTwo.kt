4bebb1a8216aa7bee717bf662bee9d17e580eaf2
febieu <febiyantiw96@gmail.com>
2023-09-28 14:03:34 +0700

    testingA

---
+++ app/src/main/java/com/example/androidstudioplayground/febi/layout/ActivityTwo.kt
@@ -0,0 +1,12 @@
+package com.example.androidstudioplayground.febi.layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidstudioplayground.R
+
+class ActivityTwo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_two)
+    }
+}
